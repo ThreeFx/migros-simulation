1eaff5dbc2281bd2c09a597a3c4f0e0deb728239
Kevin De Keyser <kdkdk@Kevins-MacBook-Air.local>
2016-11-15 16:03:24 +0100

    blub

--- src/StoreMap.java
+++ src/StoreMap.java
@@ -1,12 +1,14 @@
 public class StoreMap {
-	String[] asciiMap; //Why use strings? Well we can use colors then!
+	String[][] asciiMap; //Why use strings? Well we can use colors then!
 	
 	public void displayMap() {
 		//Note: This is fairly slow, because sysout tends not to buffer.
 		int numberOfBackspaces = 0xcafebabe; //magic
 		for(int i = 0; i < numberOfBackspaces; ++i) System.out.print('\b');
 		for(int i = 0; i < asciiMap.length; ++i) {
-				System.out.println(asciiMap[i]);
+			for(int j = 0; j < asciiMap[i].length; ++j) {
+				System.out.println(asciiMap[i][j]);
+			}
 		}	
 		System.out.flush();
 	}
