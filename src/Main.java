9716f839772080e68f847b3833e3042109da3171
Mugeeb Hassan <Gumbee@users.noreply.github.com>
2016-11-18 02:35:08 +0100

    added tiles and animation classes (#4)
    * added a map and a tiles class
    
    * added animation handler

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
-		System.out.println("Blub");		
-		String colorfulBlub = Display.makeColor("BlubExampleColor", Color.BLUE, Color.RED);
-		System.out.println(colorfulBlub);
+
+	public static void main(String[] args) {
+
+		Map.initializeMap(20, 20);
+
+		Animation animation = new Animation();
+
+		animation.run();
+
     }
 }
---
+++ src/Tile.java
@@ -0,0 +1,76 @@
+import java.util.Random;
+
+public class Tile {
+
+    // the string that will be printed out to the map
+    private char displayChar;
+
+    private int positionX;
+    private int positionY;
+
+    private Color backgroundColor;
+    private Color stringColor;
+
+    // space between the individual tiles
+    private int tilePadding = 10;
+
+    private boolean isStatic = false;
+
+    public Tile(char displayChar, int positionX, int positionY){
+        this(displayChar, positionX, positionY, true, Color.BLUE, Color.NORMAL);
+    }
+
+    public Tile(char displayChar, int positionX, int positionY, boolean isStatic){
+        this(displayChar, positionX, positionY, isStatic, Color.BLUE, Color.NORMAL);
+    }
+
+    public Tile(char displayChar, int positionX, int positionY, Color stringColor, Color backgroundColor){
+        this(displayChar, positionX, positionY, true, stringColor, backgroundColor);
+    }
+
+    public Tile(char displayChar, int positionX, int positionY, boolean isStatic, Color stringColor, Color backgroundColor){
+        this.backgroundColor = backgroundColor;
+        this.displayChar = displayChar;
+        this.stringColor = stringColor;
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.isStatic = isStatic;
+    }
+
+    /**
+     * Calculate this tile's position in the next frame in this function.
+     * This would for example be where you implement a path finding algorithm.
+     *
+     * This function must be overridden. Right now the movement is random and mainly for debugging
+     */
+    public void getNextFrame(){
+        int newX = positionX, newY = positionY;
+
+        int x;
+        int y;
+
+        boolean movementPossible = false;
+
+        while(!movementPossible) {
+            x = new Random().nextInt(3)-1;
+            y = new Random().nextInt(3)-1;
+            if (Map.canPopulate(positionX + x, positionY + y)) {
+                newX = newX + x;
+                newY = newY + y;
+                movementPossible = true;
+            }
+        }
+
+        Map.populateTile(this, newX, newY);
+        this.positionX = newX;
+        this.positionY = newY;
+    }
+
+    final public void printTile(){
+        String formatString = tilePadding>0?"%-"+tilePadding+"s":"%s";
+        System.out.print(String.format(formatString, Display.makeColor(""+displayChar, stringColor, backgroundColor) + " "));
+    }
+
+    final public boolean isStatic() { return isStatic; }
+
+}
