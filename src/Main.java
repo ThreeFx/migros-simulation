1692490d60ae334622bdb9d5dbeeae0f3d1eef05
adiba <adib1234@hotmail.de>
2016-11-19 11:17:07 +0100

    removed submodule Jansi (not working) added Jansi as jar (working fine)

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
-public class Main {
+import org.fusesource.jansi.*;
+import org.fusesource.jansi.Ansi.Color;
 
-	public static void main(String[] args) {
-
-		Map.initializeMap(20, 20);
-
-		Animation animation = new Animation();
-
-		animation.run();
-
+public class Main
+{
+    public static void main(String[] args)
+    {
+        AnsiConsole.systemInstall();
+        System.out.println( Ansi.ansi().eraseScreen().fg(Color.RED).a("Hello").fg(Color.GREEN).a(" World").reset() );
+        AnsiConsole.systemUninstall();
+        /*
+        Map.initializeMap(20, 20);
+        
+        Animation animation = new Animation();
+        
+        animation.run();
+        */
     }
 }
