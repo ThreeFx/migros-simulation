19321087f435e0674aeb4f70eaeedff797414e34
Kevin De Keyser <kdkdk@public-docking-cx-2046.ethz.ch>
2016-11-23 19:56:53 +0100

    blubiblub

--- src/Tiles/Checkout.java
+++ src/Tiles/Checkout.java
@@ -3,7 +3,7 @@ package Tiles;
 public class Checkout extends MultiTile{
 
     public Checkout(int x, int y){
-        super("Art/Checkout.ASCIIART", x, y);
+        super("Art/smallmap.ASCIIART", x, y);
 
     }
 
--- src/Tiles/Tile.java
+++ src/Tiles/Tile.java
@@ -3,6 +3,7 @@ package Tiles;
 import Screen.Color;
 import Screen.Display;
 import Screen.Map;
+import Tiles.TileType;
 
 import java.util.Random;
 
@@ -23,55 +24,69 @@ public class Tile {
     // whether the tile moves or not
     private boolean isStatic = false;
 
+	private TileType type;
+
     public Tile(char displayChar, int positionX, int positionY){
-        this(displayChar, positionX, positionY, true, Color.BLUE, Color.NOBACKGROUND);
+        this(displayChar, positionX, positionY, true, Color.BLUE, Color.NOBACKGROUND, TileType.EMPTY);
     }
 
     public Tile(char displayChar, int positionX, int positionY, boolean isStatic){
-        this(displayChar, positionX, positionY, isStatic, Color.BLUE, Color.NOBACKGROUND);
+        this(displayChar, positionX, positionY, isStatic, Color.BLUE, Color.NOBACKGROUND, TileType.EMPTY);
     }
 
     public Tile(char displayChar, int positionX, int positionY, Color stringColor, Color backgroundColor){
-        this(displayChar, positionX, positionY, true, stringColor, backgroundColor);
+        this(displayChar, positionX, positionY, true, stringColor, backgroundColor, TileType.EMPTY);
     }
 
-    public Tile(char displayChar, int positionX, int positionY, boolean isStatic, Color stringColor, Color backgroundColor){
+    public Tile(char displayChar, int positionX, int positionY, boolean isStatic, Color stringColor, Color backgroundColor, TileType type){
         this.backgroundColor = backgroundColor;
         this.displayChar = displayChar;
         this.stringColor = stringColor;
         this.positionX = positionX;
         this.positionY = positionY;
         this.isStatic = isStatic;
+		this.type = type;	
     }
 
     /**
      * Calculate this tile's position in the next frame in this function.
      * This would for example be where you implement a path finding algorithm.
      *
-     * This function must be overridden. Right now the movement is random and mainly for debugging
+	 * This function must be overridden. Right now the movement is random and mainly for debugging
      */
     public void getNextFrame(){
-        int newX = positionX, newY = positionY;
+		int newX = positionX, newY = positionY;
+       	int x;
+       	int y;
 
-        int x;
-        int y;
 
-        boolean movementPossible = false;
-
-        while (!movementPossible) {
+		switch(type) {
+			case ITEM:
+				
+			break;
+			case COUNTER:
+			break;
+			case WALL:
+			break;
+			case PERSON:
             x = new Random().nextInt(3) - 1;
             y = new Random().nextInt(3) - 1;
 
             if (Map.canPopulate(positionX + x, positionY + y, Map.nextFrameMap)) {
-                newX = newX + x;
-                newY = newY + y;
-                movementPossible = true;
-            }
-        }
+               	newX = newX + x;
+               	newY = newY + y;
+               	movementPossible = true;
+           	}
+        	
+			default:        	
+				
 
-        Map.populateTile(this, Map.nextFrameMap, newX, newY);
-        this.positionX = newX;
-        this.positionY = newY;
+       		Map.populateTile(this, Map.nextFrameMap, newX, newY);
+			this.positionX = newX;
+        	this.positionY = newY;
+        	
+
+		}
     }
 
     public void printTile(){
