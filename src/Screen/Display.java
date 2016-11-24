994f6fae438bf05dcd11d92339884648dbd7ffc8
Kevin De Keyser <kdkdk@Kevins-MacBook-Air.local>
2016-11-24 04:30:48 +0100

    DisplayOnly

--- src/Screen/Display.java
+++ src/Screen/Display.java
@@ -1,6 +1,7 @@
 package Screen;
 
 import Tiles.Tile;
+import Screen.Map;
 
 public class Display {
 
@@ -11,8 +12,12 @@ public class Display {
         for(int i = 0; i < numberOfBackspaces; ++i) System.out.print('\b');
         for(int i = 0; i < asciiMap.length; ++i) {
             for(int j = 0; j < asciiMap[i].length; ++j) {
-                if(asciiMap[i][j] != null)
-                    asciiMap[i][j].printTile();
+                if(asciiMap[i][j] != null) {
+                    if (Map.staticLayer[i][j] != null && Map.staticLayer[i][j].getDisplayCharacter() != 'X')
+                        asciiMap[i][j].printTile();
+                    //else if(Map.staticLayer[i][j] != null)
+                    //    Map.staticLayer[i][j].printTile();
+                }
                 else
                     new Tile(' ', j, i).printTile();
             }
